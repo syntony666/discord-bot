f547c0f210573468cee9a4a4f5caf49e771a4f4e
syntony666 <31161465+syntony666@users.noreply.github.com>
2023-04-20 00:32:10 +0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "Shutting down discord bot"
-docker-compose down
-echo "Deploying discord bot..."
-cd ../discord-bot
-git reset --hard
-git pull
-docker image rm node
-docker-compose up -d
-echo "Done."
