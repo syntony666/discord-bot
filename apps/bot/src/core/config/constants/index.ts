161008732f7b31784f48319684576436633e9a8f
Tony <me@syntony666.com>
2026-09-20 19:12:58 +0800

    chore(bot): drop ButtonStyles constants, use discord-api-types ButtonStyle

--- apps/bot/src/core/config/constants/index.ts
+++ apps/bot/src/core/config/constants/index.ts
@@ -1,5 +1,3 @@
 export { Timeouts } from './timeouts';
-export { ButtonStyles } from './button-styles';
-export type { ButtonStyle } from './button-styles';
 export { CustomIdPrefixes } from './custom-ids';
 export type { CustomIdPrefix } from './custom-ids';
